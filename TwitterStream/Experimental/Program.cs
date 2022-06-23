9734b97cb8b77aeb92daf5779b3fb759aa36b03f
Nick <nickwender@users.noreply.github.com>
2022-06-22 20:27:09 -0400

    feat: basic console app to stream tweets

---
+++ TwitterStream/Experimental/Program.cs
@@ -0,0 +1,65 @@
+﻿using RestSharp;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Experimental
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var twitterBaseApiUrl = "https://api.twitter.com/2";
+            var twitterStreamEndpoint = "tweets/sample/stream?tweet.fields=entities";
+            var twitterBearerToken = "";
+
+            var cancellationToken = new CancellationToken();
+
+            var client = new RestClient(twitterBaseApiUrl);
+            client.AddDefaultHeader("Authorization", $"Bearer {twitterBearerToken}");
+
+            try
+            {
+                var response = client.StreamJsonAsync<TwitterStreamResponse>(twitterStreamEndpoint, cancellationToken);
+
+                await foreach (var tweet in response.WithCancellation(cancellationToken))
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(tweet));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while streaming tweets");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    public class TwitterStreamResponse
+    {
+        public TweetObject Data { get; set; }
+    }
+
+
+    public class TweetObject
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public TweetEntity Entities { get; set; }
+
+    }
+
+    public class TweetEntity
+    {
+        public IEnumerable<TweetHashtag> Hashtags { get; set; }
+    }
+
+    public class TweetHashtag
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public string Tag { get; set; }
+    }
+}
